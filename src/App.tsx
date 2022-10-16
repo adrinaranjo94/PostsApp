9acade340b093abad631cadfb1436acf953aa0dc
Adrian <adriannaranjosanchez@gmail.com>
2022-10-16 23:33:54 +0200

    ToastContainer added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import AppRouter from '@router/AppRouter'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   return (
     <>
+      <ToastContainer />
       <AppRouter />
     </>
   )
