a74d703a9a09fb068076af0be9ec74117c2bff4f
Adrian <adriannaranjosanchez@gmail.com>
2022-10-17 11:01:02 +0200

    restructure types postsReducer

--- src/domains/posts/reducers/PostsReducer/index.ts
+++ src/domains/posts/reducers/PostsReducer/index.ts
@@ -1,39 +1,51 @@
-import { Post } from '../../models/Post'
-import * as TYPES from './types'
-
-export enum PostsStatus {
-  IDLE,
-  GET_POSTS,
-  GET_FILTERED_POSTS_BY_USER_ID,
-}
-
-export interface PostsReducerState {
-  posts: Post[]
-  isLoading: boolean
-  status: PostsStatus
-}
-
-interface PostsReducerAction {
-  type: string
-  payload: {
-    data: Post[]
-  }
-}
+import {
+  API_GET_POSTS,
+  PostsReducerAction,
+  PostsReducerState,
+  PostsStatus,
+  API_GET_FILTERED_POSTS_BY_USER_ID,
+  DOWNLOAD_POSTS,
+  DOWNLOAD_POSTS_WITH_USERS,
+  SELECT_USER_AND_FILTER,
+  DESELECT_USER,
+} from './types'
 
 export const defaultState: PostsReducerState = {
   posts: [],
-  isLoading: false,
+  users: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  userSelected: null,
+  isLoading: true,
   status: PostsStatus.IDLE,
 }
 
-export const PostsReducer = (state: PostsReducerState, { type, payload }: PostsReducerAction) => {
-  switch (type) {
-    case TYPES.API_GET_POSTS:
-      return { ...state, status: PostsStatus.GET_POSTS }
-    case TYPES.API_GET_FILTERED_POSTS_BY_USER_ID:
-      return { ...state, status: PostsStatus.GET_FILTERED_POSTS_BY_USER_ID }
-    case TYPES.DOWNLOAD_POSTS:
-      return { ...state, posts: [...payload.data], status: PostsStatus.IDLE }
+export const PostsReducer = (state: PostsReducerState, action: PostsReducerAction): PostsReducerState => {
+  switch (action.type) {
+    case API_GET_POSTS:
+      return { ...state, status: PostsStatus.GET_POSTS, isLoading: true }
+    case API_GET_FILTERED_POSTS_BY_USER_ID:
+      return { ...state, status: PostsStatus.GET_FILTERED_POSTS_BY_USER_ID, isLoading: true }
+    case DOWNLOAD_POSTS:
+      return { ...state, posts: action.payload.posts, status: PostsStatus.IDLE, isLoading: false }
+    case DOWNLOAD_POSTS_WITH_USERS:
+      return {
+        ...state,
+        posts: [...action.payload.posts],
+        users: [...new Set(action.payload.posts.map((dataObj) => dataObj.userId))],
+        status: PostsStatus.IDLE,
+        isLoading: false,
+      }
+    case SELECT_USER_AND_FILTER:
+      return {
+        ...state,
+        userSelected: action.payload.userId,
+        status: PostsStatus.GET_FILTERED_POSTS_BY_USER_ID,
+        isLoading: true,
+      }
+    case DESELECT_USER:
+      return {
+        ...state,
+        userSelected: null,
+      }
     default:
       return state
   }
--- src/domains/posts/reducers/PostsReducer/types.ts
+++ src/domains/posts/reducers/PostsReducer/types.ts
@@ -1,6 +1,53 @@
+import { Post } from '@domains/posts/models/Post'
+
 // API ACTIONS
 export const API_GET_POSTS = '[POSTS]_API_GET_POSTS'
-export const API_GET_FILTERED_POSTS_BY_USER_ID = '[POSTS] API_GET_FILTERED_POSTS_BY_USER_ID'
+export const API_GET_FILTERED_POSTS_BY_USER_ID = '[POSTS]_API_GET_FILTERED_POSTS_BY_USER_ID'
 
 // STATE ACTIONS
 export const DOWNLOAD_POSTS = '[POSTS]_DOWNLOAD_POSTS'
+export const DOWNLOAD_POSTS_WITH_USERS = '[POSTS]_DOWNLOAD_POSTS_WITH_USERS'
+export const SELECT_USER_AND_FILTER = '[POSTS]_SELECT_USER_AND_FILTER'
+export const DESELECT_USER = '[POSTS]_DESELECT_USER'
+
+export enum PostsStatus {
+  IDLE,
+  GET_POSTS,
+  GET_FILTERED_POSTS_BY_USER_ID,
+}
+
+export interface PostsReducerState {
+  posts: Post[]
+  users: number[]
+  userSelected: number | null
+  isLoading: boolean
+  status: PostsStatus
+}
+
+type ActionMap<M extends { [index: string]: any }> = {
+  [Key in keyof M]: M[Key] extends undefined
+    ? {
+        type: Key
+      }
+    : {
+        type: Key
+        payload: M[Key]
+      }
+}
+
+type PostPayload = {
+  [API_GET_POSTS]: undefined
+  [API_GET_FILTERED_POSTS_BY_USER_ID]: undefined
+  [DOWNLOAD_POSTS]: {
+    posts: Post[]
+  }
+  [DOWNLOAD_POSTS_WITH_USERS]: {
+    posts: Post[]
+  }
+  [SELECT_USER_AND_FILTER]: {
+    userId: number | null
+  }
+  [DESELECT_USER]: undefined
+}
+
+export type PostsReducerAction = ActionMap<PostPayload>[keyof ActionMap<PostPayload>]
