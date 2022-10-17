6c07eaa70837c913468ee31a9df6aef121d112dd
Adrian <adriannaranjosanchez@gmail.com>
2022-10-18 01:10:52 +0200

    change properties from button and dropdown

--- src/shared/UI/Button/index.tsx
+++ src/shared/UI/Button/index.tsx
@@ -5,10 +5,11 @@ interface ButtonProps {
   value?: string | number
   children: ReactNode
   onClick?: (e: MouseEvent<HTMLButtonElement>) => void
+  classes?: string[]
 }
-const Button = ({ value, children, onClick }: ButtonProps) => {
+const Button = ({ value, children, onClick, classes }: ButtonProps) => {
   return (
-    <button type='button' value={value} onClick={onClick} className='button'>
+    <button type='button' value={value} onClick={onClick} className={`button ${classes?.join(' ') || ''}`}>
       {children}
     </button>
   )
--- src/shared/UI/Drowdown/index.tsx
+++ src/shared/UI/Drowdown/index.tsx
@@ -1,22 +1,26 @@
 import Button from '../Button'
 import './style.scss'
 
+export interface DropdownOption {
+  key: number | null
+  text: string
+}
+
 interface DropdownProps {
-  optionSelected?: number | null
-  options: number[]
-  selectOption: (id: number | null) => void
-  emptyOption: { key: number | null; textValue: string }
+  optionSelected: number | null
+  options: DropdownOption[]
+  selectOption: (option: DropdownOption) => void
   name: string
 }
 
-const Dropdown = ({ optionSelected, options, selectOption, emptyOption, name }: DropdownProps) => {
+const Dropdown = ({ optionSelected, options, selectOption, name }: DropdownProps) => {
   return (
     <div className='dropdown'>
-      <Button>{optionSelected || emptyOption.textValue}</Button>
+      <Button>{options.find((option) => option.key === optionSelected)?.text}</Button>
       <div className='dropdown__content'>
-        {[emptyOption.key, ...options].map((option) => (
-          <p key={`dropdown-${name}-item-${option || 'null'}`} onClick={() => selectOption(option)}>
-            {option || emptyOption.textValue}
+        {[...options].map((option) => (
+          <p key={`dropdown-${name}-item-${option.key || 'null'}`} onClick={() => selectOption(option)}>
+            {option.text}
           </p>
         ))}
       </div>
