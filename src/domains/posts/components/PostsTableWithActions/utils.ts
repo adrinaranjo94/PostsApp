a2d24e83e749bff00f1f317fff85fcd4ab98894f
Adrian <adriannaranjosanchez@gmail.com>
2022-10-18 22:43:46 +0200

    implemented new utils in PostTableWithActions

--- src/domains/posts/components/PostsTableWithActions/utils.ts
+++ src/domains/posts/components/PostsTableWithActions/utils.ts
@@ -1,4 +1,5 @@
 import { DropdownOption } from '@shared/UI/Drowdown/index'
+import { KeyToSort, sortDirection, typeVar } from '../../hooks/useSortPosts'
 
 export const typesOfNumItemsPerPage: DropdownOption[] = [
   { key: 5, text: '5' },
@@ -13,3 +14,17 @@ export const defaultNumItemsPerPage = 20
 export const createOptionsFromUsersId = (users: number[]): DropdownOption[] => {
   return [{ key: null, text: 'Sin filtrar' }, ...users.map((user) => ({ key: user, text: `${user}` }))]
 }
+
+export const createArrPages = (numPages: number) => {
+  return (
+    [...Array(numPages).keys()]
+      // Create arr pages
+      .map((k) => k + 1)
+  )
+}
+
+export const keyTitleAsc: KeyToSort = { key: 'title', direction: sortDirection.asc, type: typeVar.string }
+export const keyTitleDesc: KeyToSort = { key: 'title', direction: sortDirection.desc, type: typeVar.string }
+
+export const keyUserIdAsc: KeyToSort = { key: 'userId', direction: sortDirection.asc, type: typeVar.string }
+export const keyUserIdDesc: KeyToSort = { key: 'userId', direction: sortDirection.desc, type: typeVar.string }
