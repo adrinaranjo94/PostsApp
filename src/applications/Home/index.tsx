e5fa152cc5d30bca33db0f0a817c07e0f2f9cbbc
Adrian <adriannaranjosanchez@gmail.com>
2022-10-19 00:13:42 +0200

    route modified

--- src/applications/Home/index.tsx
+++ src/applications/Home/index.tsx
@@ -1,11 +1,11 @@
 import { Link } from 'react-router-dom'
-import { ROUTE_POSTS_LIST, getPostsRouteByKey } from '@router/route/posts/index'
+import { KEY_ROUTE_POSTS_LIST, getPostsRouteByKey } from '@router/route/posts/index'
 
 const Home = () => {
   return (
     <div>
       Home
-      <Link to={getPostsRouteByKey(ROUTE_POSTS_LIST)}> POSTS </Link>
+      <Link to={getPostsRouteByKey(KEY_ROUTE_POSTS_LIST)}> POSTS </Link>
     </div>
   )
 }
