252d885a9c372cdaf67667bf6dba77b9b8d15537
Adrian <adriannaranjosanchez@gmail.com>
2022-10-19 18:53:13 +0200

    change styles home page

--- src/applications/Home/index.tsx
+++ src/applications/Home/index.tsx
@@ -1,11 +1,20 @@
 import { Link } from 'react-router-dom'
 import { KEY_ROUTE_POSTS_LIST, getPostsRouteByKey } from '@router/route/posts/index'
+import Button from '@shared/UI/Button'
+import './style.scss'
 
 const Home = () => {
   return (
-    <div>
-      Home
-      <Link to={getPostsRouteByKey(KEY_ROUTE_POSTS_LIST)}> POSTS </Link>
+    <div className='homePage'>
+      <h1>Home</h1>
+
+      <h2>Prueba técnica</h2>
+      <h2>Adrian Naranjo</h2>
+      <hr />
+      <h3>Proyecto creado en Vite + React / TS</h3>
+      <Button>
+        <Link to={getPostsRouteByKey(KEY_ROUTE_POSTS_LIST)}> POSTS </Link>
+      </Button>
     </div>
   )
 }
