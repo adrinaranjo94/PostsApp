e61d92a2f0facc027a8ddef8ff8b341521ed9bbe
Adrian <adriannaranjosanchez@gmail.com>
2022-10-19 17:33:05 +0200

    button component restructured

--- src/shared/UI/Button/index.tsx
+++ src/shared/UI/Button/index.tsx
@@ -6,10 +6,18 @@ interface ButtonProps {
   children: ReactNode
   onClick?: (e: MouseEvent<HTMLButtonElement>) => void
   classes?: string[]
+  disabled?: boolean
+  type?: 'button' | 'submit' | 'reset'
 }
-const Button = ({ value, children, onClick, classes }: ButtonProps) => {
+const Button = ({ value, children, onClick, classes, disabled, type }: ButtonProps) => {
   return (
-    <button type='button' value={value} onClick={onClick} className={`button ${classes?.join(' ') || ''}`}>
+    <button
+      type={type || 'button'}
+      disabled={disabled}
+      value={value}
+      onClick={onClick}
+      className={`button ${classes?.join(' ') || ''}`}
+    >
       {children}
     </button>
   )
