9faca8c56be7ab1f7e97a40d56a7c1ec5e255527
Adrian <adriannaranjosanchez@gmail.com>
2022-10-19 18:19:37 +0200

    link from navbar

--- src/shared/UI/NavBar/index.tsx
+++ src/shared/UI/NavBar/index.tsx
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom'
 import './style.scss'
+import { getPostsRouteByKey, KEY_ROUTE_POSTS_LIST } from '@router/route/posts/index'
 
 const NavBar = () => {
   return (
     <div className='navbar'>
-      <p className='navbar__title'>PostsApp</p>
+      <Link to={getPostsRouteByKey(KEY_ROUTE_POSTS_LIST)} className='navbar__title'>
+        PostsApp
+      </Link>
     </div>
   )
 }
